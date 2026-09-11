5852a6a19122a8bac037b1d8d7f05e8db7bfa6be
ritesh90256 <riteshminchinal365@gmail.com>
2026-09-11 12:52:50 +0530

    perf: benchmark horizontal scaling

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,7 +1,12 @@
+import os
+
 from redis import Redis
 
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", "6379"))
+
 redis_client = Redis(
-    host = "localhost",
-    port = 6379,
+    host=redis_host,
+    port=redis_port,
     decode_responses=True
 )
